a3b0af4cf4090d16301f02df6d1a556f80d9b973
choc0bot <choc0bot@gmail.com>
2015-06-16 16:42:39 +1000

    toggle buttons

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/test')
+def test():
+    return render_template('test.html', title='test')
