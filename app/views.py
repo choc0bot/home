323b4d1540ab113c634638c990b4c2f09d48143e
choc0bot <choc0bot@gmail.com>
2015-06-17 21:27:33 +1000

    local changes

--- app/views.py
+++ app/views.py
@@ -5,8 +5,14 @@ import urllib
 @app.route('/')
 @app.route('/index')
 def index():
+    bed1_initial_class="btn-danger"
     state_bed1 = urllib.urlopen('http://192.168.1.101/cgi-bin/relay.cgi?state').read()
-    return render_template('index.html', title='Home', state_bed1=state_bed1)
+    state_bed1 = state_bed1.strip()
+    if state_bed1 == "ON":
+        bed1_initial_class="btn-success"
+    return render_template('index.html', title='Home',
+                                         bed1_initial_class=bed1_initial_class,
+                                         state_bed1=state_bed1)
 
 @app.route('/testbed2')
 def test():
