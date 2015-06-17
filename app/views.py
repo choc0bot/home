2dcbc53cd48b49e4bc70202a50fadc1ef96349b5
choc0bot <choc0bot@gmail.com>
2015-06-17 16:51:18 +1000

    ui tweaks

--- app/views.py
+++ app/views.py
@@ -8,6 +8,6 @@ def index():
     state_bed1 = urllib.urlopen('http://192.168.1.101/cgi-bin/relay.cgi?state').read()
     return render_template('index.html', title='Home', state_bed1=state_bed1)
 
-@app.route('/test')
+@app.route('/testbed2')
 def test():
-    return render_template('test.html', title='test')
+    return render_template('testbed2.html', title='testbed2')
