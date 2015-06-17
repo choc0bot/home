14bc5a51d350e6fedd5f9b94ed429fe8c48ca21a
choc0bot <choc0bot@gmail.com>
2015-06-17 12:18:55 +1000

    Added initial state to view.py

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from app import app
 from flask import render_template
+import urllib
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    state_bed1 = urllib.urlopen('http://192.168.1.101/cgi-bin/relay.cgi?state').read()
+    return render_template('index.html', title='Home', state_bed1=state_bed1)
 
 @app.route('/test')
 def test():
